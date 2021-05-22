f7fc72e2e30ace7acb5f8c4288ef653c55e297a3
rami11 <rami.serapian@gmail.com>
2021-05-22 18:35:15 -0400

    Update setup script

--- setup.sh
+++ setup.sh
@@ -6,30 +6,34 @@ BACKUP_DIR=~/dotfiles.bak
 [ -d $BACKUP_DIR ] || mkdir $BACKUP_DIR
 
 [ -f ~/.bashrc ]        && mv -f ~/.bashrc        $BACKUP_DIR
-[ -f ~/.vimrc ]         && mv -f ~/.vimrc         $BACKUP_DIR
-[ -f ~/.vimrc.bundles ] && mv -f ~/.vimrc.bundles $BACKUP_DIR
+[ -f ~/.inputrc ]       && mv -f ~/.inputrc       $BACKUP_DIR
+# [ -f ~/.vimrc ]         && mv -f ~/.vimrc         $BACKUP_DIR
+# [ -f ~/.vimrc.bundles ] && mv -f ~/.vimrc.bundles $BACKUP_DIR
 [ -f ~/.tmux.conf ]     && mv -f ~/.tmux.conf     $BACKUP_DIR
 [ -f ~/.bash_aliases ]  && mv -f ~/.bash_aliases  $BACKUP_DIR
 [ -f ~/.gitconfig ]  	&& mv -f ~/.gitconfig     $BACKUP_DIR
-[ -f ~/.emacs.d ]  	&& mv -f ~/.emacs.d       $BACKUP_DIR
+[ -f ~/.emacs.default ] && mv -f ~/.emacs.default $BACKUP_DIR
 
 # Remove symbolic links to target files if they exist
 [ -L ~/.bashrc ]        && rm ~/.bashrc
-[ -L ~/.vimrc ]         && rm ~/.vimrc
-[ -L ~/.vimrc.bundles ] && rm ~/.vimrc.bundles
+[ -L ~/.inputrc ]       && rm ~/.inputrc
+# [ -L ~/.vimrc ]         && rm ~/.vimrc
+# [ -L ~/.vimrc.bundles ] && rm ~/.vimrc.bundles
 [ -L ~/.tmux ]          && rm ~/.tmux.conf
 [ -L ~/.bash_aliases ]  && rm ~/.bash_aliases
 [ -L ~/.gitconfig ]  	&& rm ~/.gitconfig
-[ -L ~/.emacs.d ]  	&& rm ~/.emacs.d
+[ -L ~/.emacs.default ] && rm ~/.emacs.default
 
 # Symlink config files with target files
 ln -s ~/dotfiles/bashrc         ~/.bashrc
-ln -s ~/dotfiles/vimrc          ~/.vimrc
-ln -s ~/dotfiles/vimrc.bundles  ~/.vimrc.bundles
+ln -s ~/dotfiles/inputrc        ~/.inputrc
+# ln -s ~/dotfiles/vimrc          ~/.vimrc
+# ln -s ~/dotfiles/vimrc.bundles  ~/.vimrc.bundles
 ln -s ~/dotfiles/tmux.conf      ~/.tmux.conf
 ln -s ~/dotfiles/aliases        ~/.bash_aliases
 ln -s ~/dotfiles/gitconfig      ~/.gitconfig
-ln -s ~/dotfiles/emacs.d        ~/.emacs.d
+ln -s ~/dotfiles/emacs.d        ~/.emacs.default
 
 # Update the shell
-[ -f ~/.bashrc ] && source ~/.bashrc
+[ -f ~/.bashrc ]  && source ~/.bashrc
+[ -f ~/.inputrc ] && source ~/.inputrc
