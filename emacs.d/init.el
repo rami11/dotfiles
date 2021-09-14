51b0b0f147df17ad154f37463d0016484e43f7c8
rami11 <rami.serapian@gmail.com>
2021-09-13 21:18:14 -0400

    Update emacs.d/init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -12,18 +12,19 @@
 (blink-cursor-mode 0)
 (show-paren-mode)
 
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+;;(projectile-mode +1)
+;;(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+;;(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes nil)
+ '(custom-enabled-themes (quote (wheatgrass)))
  '(package-selected-packages
-   '(kotlin-mode treemacs angular-mode react-snippets yasnippet projectile cider magit)))
+   (quote
+    (kotlin-mode treemacs angular-mode react-snippets yasnippet projectile cider magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
